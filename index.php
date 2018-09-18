037b37bfb623a5baadc8e8bf2f0e5929a9d9a323
hkt9x <bkit9x@gmail.com>
2018-09-18 21:47:20 +0700

    cache

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 	<meta charset="UTF-8">
 	<title>Nguyễn Hoàng Kha</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="sheet/bootstrap.css?v=1">
-	<link rel="stylesheet" href="sheet/mine.css?v=1">
+	<link rel="stylesheet" href="sheet/bootstrap.css">
+	<link rel="stylesheet" href="sheet/mine.css">
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,700" rel="stylesheet">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 	<script src="javascript/myscript.js"></script>
