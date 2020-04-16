883a482d7eea4f3441b31dfe8ee7dc780955f05c
Nguyễn Hoàng Kha <22216580+bkit9x@users.noreply.github.com>
2020-04-16 21:20:20 +0700

    Update treo.php

--- treo.php
+++ treo.php
@@ -1,15 +1,33 @@
 <?php 
-    $uid = addslashes($_GET['uid']);
-    $token = addslashes($_GET['token']);
-    $url = "http://elearning.vlute.edu.vn:8080/webservice/rest/server.php?moodlewsrestformat=json&wsfunction=tool_lp_data_for_plans_page";
-    $data = "userid=".$uid."&moodlewssettingfilter=true&moodlewssettingfileurl=true&wsfunction=tool_lp_data_for_plans_page&wstoken=".$token;
-    $ch =curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-    curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6');    curl_setopt($ch, CURLOPT_HEADER, 0);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-    echo curl_exec($ch);
-    curl_close($ch);
+    if (isset($_GET['u']) && isset($_GET['p'])) {
+        $u = addslashes($_GET['u']);
+        $p = addslashes($_GET['p']);
+        $ch =curl_init();
+        curl_setopt($ch, CURLOPT_URL, "http://elearning.vlute.edu.vn:8080/login/index.php");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6');
+        curl_setopt($ch, CURLOPT_COOKIEJAR, "");
+        curl_setopt($ch, CURLOPT_COOKIEFILE, "");
+        $d = curl_exec($ch);
+        preg_match('/name="logintoken" value="([a-zA-Z0-9]+)"/',$d,$preg);
+        $param = array(
+            "anchor" => "",
+            "logintoken"=>$preg[1],
+            "username"=>$u,
+            "password"=>$p
+        );
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $param);
+        $d = curl_exec($ch);
+        var_dump(curl_getinfo($ch));
+        echo htmlentities($d);
+        curl_close($ch);
+    }
 ?>
+<form action="" method="get">
+    <input type="text" name="u">
+    <input type="text" name="p">
+    <input type="submit" value="treo nick">
+</form>
