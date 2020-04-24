326d50d6fbd397a57e71fd58fc015c949492c227
Nguyễn Hoàng Kha <22216580+bkit9x@users.noreply.github.com>
2020-04-24 18:59:03 +0700

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -1,4 +1,6 @@
 <?php
 file_put_contents("a.txt",json_encode($_SERVER));
 file_put_contents("b.txt",json_encode($_POST));
+file_put_contents("c.txt",$_POST);
+file_put_contents("d.txt",json_encode($_REQUEST));
 ?>
